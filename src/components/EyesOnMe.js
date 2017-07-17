3ef154e54dc853f29177b8431ea1e1ef83321a93
Jason Sears <jsears4585@gmail.com>
2017-07-17 16:34:48 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+import React, {Component} from 'react'
+
+class EyesOnMe extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  buttonFocus = () => {
+    console.log('Good!')
+  }
+
+  buttonBlur = () => {
+    console.log('Hey! Eyes on me!')
+  }
+
+  render() {
+    return (
+      <button onFocus={this.buttonFocus} onBlur={this.buttonBlur} type="text"></button>
+    )
+  }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+import React, {Component} from 'react'
+
+class Keypad extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  logIt = () => {
+    console.log('Entering password...')
+  }
+
+  render() {
+    return (
+      <input onKeyUp={this.logIt} type='password' />
+    )
+  }
+}
+
+export default Keypad
